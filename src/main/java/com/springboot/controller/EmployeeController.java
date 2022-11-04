384502d264378eb56aa9b6ed5f2da5e4c1d6d819
Ashwini <ashwinilakshmanan1@gmail.com>
2022-11-05 00:13:55 +0530

    delete employee rest api

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,5 +81,15 @@ public class EmployeeController {
 		return ResponseEntity.ok(updateEmployee);
 	}
 	
+	//build delete employeeREST API
+	@DeleteMapping("{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id) throws ResourceNotFoundException{
+		
+		Employee employee=employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id:" +id));
+		
+		employeeRepository.delete(employee);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 	
 }
